389be3396fbf08f0efe52968f188f97ea331c4de
cheneeheng <eeheng.chen@gmail.com>
2021-10-15 14:42:11 +0200

    Preparing to generate data.

--- mimic-iv/projects/common.py
+++ mimic-iv/projects/common.py
@@ -5,6 +5,8 @@ MP_CHUNK_SIZE = 1
 
 """Working with database """
 DB_EXPORT_PATH = "/data/raw_json_data_v1"
+STRUCTURED_EXPORT_DIR = "/data/structured_dsv_data_v1"
 
 """Resources """
 DATA_ITEM_PATH = "mimic-iv/projects/resources/Mimic4DataItems.tsv"
+UID_INFO_PATH = "mimic-iv/projects/resources/uid_info.json"
